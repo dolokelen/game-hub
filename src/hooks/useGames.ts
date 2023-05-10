514abceb2559b6d4e95990b565a98ef5b7909e1a
dolokelen <dolokelen1@gmail.com>
2023-05-11 07:53:08 +0800

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,12 +6,13 @@ export interface Platform {
     id: number;
     name: string;
     slug: string;
-}
-export interface Game {
+  }
+  export interface Game {
     id: number;
     name: string;
     background_image: string;
     parent_platforms:{platform: Platform}[];
+    metacritic: number;
   }
   
   interface FetchGameResponse {
