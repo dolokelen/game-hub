513884b2cc1062e2d97766e6c63031b5725212bc
dolokelen <dolokelen1@gmail.com>
2023-09-21 06:19:53 +0800

    Refactor- Move entities interfaces

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { NavBar } from "../component/NavBar";
-import { Outlet } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import { NavBar } from "../component/NavBar";
 
 const Layout = () => {
   return (
