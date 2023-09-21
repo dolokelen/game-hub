a3cd0081a037800a824bb32906c524b8080c83e2
dolokelen <dolokelen1@gmail.com>
2023-09-21 09:06:07 +0800

    Built Expandable Text

---
+++ src/pages/ExpandableText.tsx
@@ -0,0 +1,34 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface Props {
+  children: string;
+}
+const ExpandableText = ({ children }: Props) => {
+  const [isExpanded, setExpanded] = useState(false);
+  const maxChars = 300;
+
+  if (!children) return null;
+
+  if (children.length <= maxChars) return <Text>{children}</Text>;
+
+  const summary = isExpanded
+    ? children.substring(0, maxChars) + "..."
+    : children;
+  return (
+    <Text>
+      {summary}{" "}
+      <Button
+        size="xs"
+        marginLeft={1}
+        fontWeight="bold"
+        colorScheme="yellow"
+        onClick={() => setExpanded(!isExpanded)}
+      >
+        {isExpanded ? "Read more" : "Read less"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,18 +1,19 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
+import ExpandableText from "./ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
-  const { data: game, isLoading, error } = useGame(slug!); //slug will never be null
+  const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
-  if (error || !game) throw error; //We're avoiding optinal chining of game
+  if (error || !game) throw error;
 
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
